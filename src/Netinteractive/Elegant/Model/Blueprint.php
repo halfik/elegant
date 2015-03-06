be5ba24540a6bdc8e546e4d0fde7beeda42f4574
halfik <halfik@gmail.com>
2015-03-06 15:21:23 +0100

    Model + Blueprint

--- src/Netinteractive/Elegant/Model/Blueprint.php
+++ src/Netinteractive/Elegant/Model/Blueprint.php
@@ -12,7 +12,15 @@ namespace Netinteractive\Elegant\Model;
  * Class Blueprint
  * @package Netinteractive\Elegant\Model
  */
-abstract class Blueprint {
+abstract class Blueprint
+{
+
+    /**
+     * Blueprint instance
+     * @var null|Netinteractive\Elegant\Model\Blueprint
+     */
+    protected static $intance = null;
+
     /**
      * @var array
      */
@@ -23,9 +31,59 @@ abstract class Blueprint {
      */
     protected $relations = array();
 
+    /**
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * @var array
+     */
+    protected $primaryKey = array();
+
+
+    /**
+     * @var bool
+     */
+    protected $validationEnabled = true;
+
+
+    /**
+     * Constructor
+     */
+    protected function __construct()
+    {
+        $this->init();
+    }
+
+
+    /**
+     * Creates Blueprint instance
+     * @return Blueprint|Netinteractive\Elegant\Model\Blueprint|null
+     */
+    static public function getInstance()
+    {
+        if (!self::$intance){
+            self::$intance = new self();
+        }
+
+        return self::$intance;
+    }
+
+
+    /**
+     * Initialize blueprint
+     * @return $this
+     */
+    protected function init()
+    {
+        return $this;
+    }
+
 
     /**
      * Returns list of fields
+     *
      * @return array
      */
     public function getFields()
@@ -35,6 +93,7 @@ abstract class Blueprint {
 
     /**
      * Returns list of fields that are sortable
+     *
      * @return array
      */
     public function getSortableFields()
@@ -44,6 +103,7 @@ abstract class Blueprint {
 
     /**
      * Returns list of fields which can be searched
+     *
      * @return array
      */
     public function getSearchableFields()
@@ -52,6 +112,8 @@ abstract class Blueprint {
     }
 
     /**
+     * Returns list of fields titles
+     *
      * @return
      */
     public function getFieldsTitles()
@@ -59,21 +121,165 @@ abstract class Blueprint {
 
     }
 
+    /**
+     * Returns field title
+     *
+     * @param string $field
+     * @return string
+     */
+    public function getFieldTitle($field)
+    {
+
+    }
 
     /**
+     * Return list of fields validation rules
      *
-    + getFieldsTitles(): array
-    + getFieldTitle(field: string): string|null
-    + getFieldRules(field: string): array
-    + getFieldsRules (rulesGroups: array, fields: array): array
-    + getFieldsTypes (fields: array): array
-    + getFieldType (field: string): string|null
-    + setFieldRules (field: string, rules: array, group: string): this
-    + isInFields (field: string): boolean
-
-
-    + enableValidation (): this
-    + disableValidation (): this
-
+     * @param string|array $rulesGroups
+     * @param array $fields
+     * @return array
      */
+    public function getFieldsRules($rulesGroups='all', $fields=array())
+    {
+
+    }
+
+    /**
+     * Return list of specific field validation rules
+     *
+     * @param string $field
+     * @return array
+     */
+    public function getFieldRules($field)
+    {
+
+    }
+
+    /**
+     * Returns list of fields types
+     *
+     * @param array $fields
+     * @return array
+     */
+    public function getFieldsTypes($fields = array())
+    {
+
+    }
+
+    /**
+     * Returns specific field type
+     *
+     * @param string $field
+     * @return string|null
+     */
+    public function getFieldType($field)
+    {
+
+    }
+
+    /**
+     * Set validation rules for a field
+     *
+     * @param string $field
+     * @param array $rules
+     * @param string $group
+     *
+     * @return $this
+     */
+    public function setFieldRules($field, array $rules, $group='all')
+    {
+
+        return $this;
+    }
+
+    /**
+     * Checks if field exists in the fields list
+     *
+     * @param string $field
+     * @return boolean
+     */
+    public function isField($field)
+    {
+        return false;
+    }
+
+    /**
+     * Returns table name
+     * @return string
+     */
+    public function getTable()
+    {
+        return $this->table;
+    }
+
+    public function setTable($name)
+    {
+        $this->table = $name;
+        return $this;
+    }
+
+    /**
+     * Returns table primary key. Can contains multiple fields
+     * @return array
+     */
+    public function getPrimaryKey()
+    {
+        return $this->primaryKey;
+    }
+
+    /**
+     * Sets primary key
+     * @param string|array $key
+     * @return $this
+     */
+    public function setPrimaryKey($key)
+    {
+        if (!is_array($key)){
+            $key = array($key);
+        }
+
+        $this->primaryKey = $key;
+        return $this;
+    }
+
+    /**
+     * Return list of relations
+     * @return array
+     */
+    public function getRelations()
+    {
+        return $this->relations;
+    }
+
+    /**
+     * Get a specified relationship.
+     *
+     * @param  string  $relation
+     * @return array
+     */
+    public function getRelation($relation)
+    {
+        return $this->relations[$relation];
+    }
+
+
+    /**
+     * Enables fields validations rules
+     * @return $this
+     */
+    public function enableValidation()
+    {
+        $this->validationEnabled = true;
+        return $this;
+    }
+
+    /**
+     * Disabled fields validations rules
+     * @return $this
+     */
+    public function disableValidation()
+    {
+        $this->validationEnabled = false;
+        return $this;
+    }
 } 
--- src/Netinteractive/Elegant/Model/Model.php
+++ src/Netinteractive/Elegant/Model/Model.php
@@ -12,6 +12,312 @@ namespace Netinteractive\Elegant\Model;
  * Class Model
  * @package Netinteractive\Elegant\Model
  */
-abstract class Model {
+abstract class Model
+{
+    /**
+     * @var Netinteractive\Elegant\Model\Blueprint
+     */
+    protected $blueprint;
+
+    /**
+     * Model attributes
+     * @var array
+     */
+    protected $attributes = array();
+
+    /**
+     * Model external attributes (attributes that dosnt normaly belong to this model)
+     * @var array
+     */
+    protected $external = array();
+
+    /**
+     * Model original attributes (before any changes were made)
+     * @var array
+     */
+    protected $original = array();
+
+
+
+    /**
+     * Create a new Model instance.
+     *
+     * @param  array  $attributes
+     * @return void
+     */
+    public function __construct(array $attributes = array())
+    {
+        $this->fill($attributes);
+    }
+
+
+    /**
+     * Fills model with data
+     * @param array $attributes
+     * @return $this
+     */
+    public function fill(array $attributes)
+    {
+        foreach ($attributes as $key => $value)
+        {
+            $this->$key = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Sets model blueprint
+     *
+     * @param Netinteractive\Elegant\Model\Blueprint $blueprint
+     * @return $this
+     */
+    public function setBlueprint(Netinteractive\Elegant\Model\Blueprint $blueprint)
+    {
+        $this->blueprint = $blueprint;
+        return $this;
+    }
+
+    /**
+     * Returns model blueprint
+     *
+     * @return Netinteractive\Elegant\Model\Blueprint
+     */
+    public function getBlueprint()
+    {
+        return $this->blueprint;
+    }
+
+    /**
+     * Set a given attribute on the model
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return $this
+     */
+    public function setAttribute($key, $value)
+    {
+
+        #we check if data if model attribute or not
+        if ($this->getBlueprint()->isField($key)){
+            $this->attributes[$key] = $value;
+        }else{
+            $this->external[$key] = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get an attribute from the model.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function getAttribute($key)
+    {
+        return $this->attributes[$key];
+    }
+
+    /**
+     * Get all of the current attributes on the model
+     *
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * Checks if field is as attribute
+     *
+     * @param $field
+     * @return bool
+     */
+    public function isAttribute($field)
+    {
+        return array_key_exists($field,$this->attributes);
+    }
+
+    /**
+     * Return list of external attributes (attributes that don't belong to this model)
+     * @return array
+     */
+    public function getExternals()
+    {
+        return $this->external;
+    }
+
+
+
+    /**
+     * Determine if the model or given attribute(s) have been modified.
+     *
+     * @param  array|string|null  $attributes
+     * @return bool
+     */
+    public function isDirty($attributes = null)
+    {
+        $dirty = $this->getDirty();
+
+        if (is_null($attributes)) return count($dirty) > 0;
+
+        if ( ! is_array($attributes)) $attributes = func_get_args();
+
+        foreach ($attributes as $attribute)
+        {
+            if (array_key_exists($attribute, $dirty)) return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the attributes that have been changed since last sync.
+     *
+     * @return array
+     */
+    public function getDirty()
+    {
+        $dirty = array();
+
+        foreach ($this->attributes as $key => $value)
+        {
+            if ( ! array_key_exists($key, $this->original))
+            {
+                $dirty[$key] = $value;
+            }
+            elseif ($value !== $this->original[$key] &&
+                ! $this->originalIsNumericallyEquivalent($key))
+            {
+                $dirty[$key] = $value;
+            }
+        }
+
+        return $dirty;
+    }
+
+    /**
+     * Determine if the new and old values for a given key are numerically equivalent.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    protected function originalIsNumericallyEquivalent($key)
+    {
+        $current = $this->attributes[$key];
+
+        $original = $this->original[$key];
+
+        return is_numeric($current) && is_numeric($original) && strcmp((string) $current, (string) $original) === 0;
+    }
+
+
+    /**
+     * Sync the original attributes with the current.
+     *
+     * @return $this
+     */
+    public function syncOriginal()
+    {
+        $this->original = $this->attributes;
+
+        return $this;
+    }
+
+
+    /**
+     * Convert the model instance to JSON.
+     *
+     * @param  int  $options
+     * @return string
+     */
+    public function toJson($options = 0)
+    {
+        return json_encode($this->toArray(), $options);
+    }
+
+    /**
+     * Convert the object into something JSON serializable.
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Convert the model instance to an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $attributes = $this->attributes;
+        
+
+        return $attributes;
+    }
+
+
+
+
+    /**
+     * Convert the model to its string representation.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->toJson();
+    }
+
+
+    /**
+     * Dynamically retrieve attributes on the model.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->getAttribute($key);
+    }
+
+    /**
+     * Dynamically set attributes on the model.
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return void
+     */
+    public function __set($key, $value)
+    {
+        $this->setAttribute($key, $value);
+    }
+
+    /**
+     * Determine if an attribute exists on the model.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function __isset($key)
+    {
+        return ((isset($this->attributes[$key]) || isset($this->external[$key])) );
+    }
+
+    /**
+     * Unset an attribute on the model.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    public function __unset($key)
+    {
+        unset($this->attributes[$key], $this->external[$key]);
+    }
 
 } 
