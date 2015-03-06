2ac4b1e603eacb85ef502431483d00ff83bf2e40
smike <michal.szapowalow@netinteractive.pl>
2015-03-06 16:45:33 +0100

    fix namespaces

--- src/Netinteractive/Elegant/Model/MapperInterface.php
+++ src/Netinteractive/Elegant/Model/MapperInterface.php
@@ -8,10 +8,60 @@
 
 namespace Netinteractive\Elegant\Model;
 
+use Netinteractive\Elegant\Model\Model;
+
 /**
  * Interface MapperInterface
  * @package Netinteractive\Elegant\Model
  */
 interface MapperInterface {
+
+    /**
+     * Delete record
+     *
+     * @param integer $id
+     * @return $this
+     */
+    function delete($id);
+
+    /**
+     * Save model
+     *
+     * @param Model $model
+     * @return $this
+     */
+    function save(Model $model);
+
+    /**
+     * Find one model
+     *
+     * @param $id
+     * @param array $columns
+     * @return Model
+     */
+    function find($id, array $columns);
+
+    /**
+     * Find collection of models
+     *
+     * @param array $params
+     * @return mixed
+     */
+    function findMany(array $params);
+
+    /**
+     * Create new model
+     *
+     * @param array $data
+     * @return Model
+     */
+    function createModel(array $data = array());
+
+    /**
+     * get name of moles class
+     *
+     * @return string
+     */
+    function getModelName();
 
 } 
