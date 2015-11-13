2a0daf36d36bc35249ff4f29c0df4c401fd6a899
halfik <halfik@gmail.com>
2015-11-13 10:54:45 +0100

    @dev

--- src/Netinteractive/Elegant/helpers.php
+++ src/Netinteractive/Elegant/helpers.php
@@ -5,8 +5,10 @@ if (! function_exists('classDotNotation')) {
      * @param object $obj
      * @return string
      */
-    function classDotNotation( $obj)
+    function app_path( $obj)
     {
         return strtolower(str_replace('\\', '.', get_class($obj)));
     }
+}else{
+    trigger_error('unction classDotNotation already exists');
 }
