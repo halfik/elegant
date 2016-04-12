d091d3b35dc44617cca81c2b3ab4ba18e66a4e2a
halfik <halfik@gmail.com>
2016-04-12 10:34:30 +0200

    @dev

---
+++ src/Netinteractive/Elegant/Http/CrutTrait.php
@@ -0,0 +1,53 @@
+<?php namespace Netinteractive\Elegant\Http;
+
+
+/**
+ * Class CrutTrait
+ * @package Netinteractive\Elegant\Http
+ */
+trait CrutTrait
+{
+    /**
+     * @var \Netinteractive\Elegant\Model\Provider
+     */
+    protected $domainProvider;
+
+
+    public function create(array $params=array())
+    {
+
+    }
+
+    public function update(array $params=array())
+    {
+
+    }
+
+    public function find(array $params=array())
+    {
+
+    }
+
+    public function delete(array $params=array())
+    {
+
+    }
+
+    /**
+     * Returns model domain provider
+     * @return \Netinteractive\Elegant\Model\Provider|void
+     */
+    public function getProvider()
+    {
+        if(is_null($this->domainProvider)){
+            $this->domainProvider = $this->createDomainProvider();
+        }
+        return $this->domainProvider;
+    }
+
+    /**
+     * Method delivers model domain provider
+     * @return \Netinteractive\Elegant\Model\Provider
+     */
+    abstract protected function createDomainProvider();
+}
